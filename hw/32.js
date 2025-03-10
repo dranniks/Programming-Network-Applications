79dc105496f6feb0f4bc006b219eb8da3601a040
andrrewterry <andrrewterry@gmail.com>
2025-03-11 00:02:17 +0300

    structured folders

--- hw/32.js
+++
@@ -1,9 +0,0 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let average = 0;
-
-for (let i = 0; i < numbers.length; i++) {
-    average += numbers[i];
-}
-
-average /= numbers.length;
-console.log(average);
